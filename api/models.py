4bb2d12ba91df4afaa3d69838d9af6eb8c7d414e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-09-27 17:03:46 +0300

    Add User model class

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+from django.core.validators import MaxLengthValidator,MinLengthValidator
+from cloudinary.models import CloudinaryField
+from django.db.models.deletion import SET_NULL,CASCADE
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+class User(AbstractUser):
+    full_name = models.CharField(max_length=144,blank=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+    profile_picture = ImageField(upload_to='profiles')
+    phone = models.CharField(max_length=13, null=True,blank=True, validators=[MinLengthValidator(10),MaxLengthValidator(13)])
+
+    def save_user(self):
+        self.save()
+
+    def delete_user(self):
+        self.delete()
+
+    def __str__(self):
+        return self.username
